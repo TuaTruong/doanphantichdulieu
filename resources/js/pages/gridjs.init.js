c09d096517fbad1dfd181ec64d9a63d0b83a0681
unknown <you@example.com>
2024-09-19 18:04:29 +0700

    skjdfhskdhf

--- resources/js/pages/gridjs.init.js
+++ resources/js/pages/gridjs.init.js
@@ -32,7 +32,7 @@ if (document.querySelector("#add_link_xoi_lac")) {
             // Insert HTML for the two halves
             container.insertAdjacentHTML('beforeend', `
                 <div class="col-3 ${data_delete_match}">
-                    <button class="btn btn-danger waves-effect waves-light delete-match ${data_delete_match}" style="display:inline" type="button">Xoá</button>
+                    <button class="btn btn-danger waves-effect waves-light delete-match ${data_delete_match}" style="width: 100%" type="button">Xoá</button>
                     <div class="card">
                         <div class="card-header">
                             <h4 class="card-title mb-0 flex-grow-1" >${team_home} - ${team_away} hiệp 1</h4>
@@ -59,10 +59,10 @@ if (document.querySelector("#add_link_xoi_lac")) {
             // Initialize grid configurations
             const initializeTable = (elementId) => new gridjs.Grid({
                 columns: [
-                    { name: "Chỉ số", width: "90px" },
-                    { name: team_home, width: "100px" },
-                    { name: team_away, width: "100px" },
-                    { name: "Tổng" }
+                    { name: "Chỉ số", id:"index", width: "70px" },
+                    { name: team_home, id:"team_home", width: "90px" },
+                    { name: team_away, id:"team_away", width: "90px" },
+                    { name: "Tổng", id:"total"}
                 ],
                 data: [
                     [`Tổng (${total_stats_indices})`, "loading", "loading", "loading"],
@@ -77,24 +77,39 @@ if (document.querySelector("#add_link_xoi_lac")) {
 
             // Function to update the tables
             const updateTables = (response) => {
-                document.querySelector(`#${table_id_half1}`).parentElement.parentElement.querySelector("h4").innerText = `${response.team_home} - ${response.team_away} hiệp 1 (Phút thứ ${response.current_time})`
+                let current_time = parseInt(response.current_time.match(/\d+/g)[0]);
+                document.querySelector(`#${table_id_half1}`).parentElement.parentElement.querySelector("h4").innerText = `${response.team_home} - ${response.team_away} hiệp 1 (Phút thứ ${current_time >= 45 ? 45 : current_time})`
                 document.querySelector(`#${table_id_half2}`).parentElement.parentElement.querySelector("h4").innerText = `${response.team_home} - ${response.team_away} hiệp 2 (Phút thứ ${response.current_time})`
                 let stats = response.statistics;
-                tableHalf1.updateConfig({
-                    data: [
-                        [`Tổng (${total_stats_indices})`, stats.total_home_stats_half1, stats.total_away_stats_half1, stats.total_home_stats_half1 + stats.total_away_stats_half1],
-                        ["Tấn công nguy hiểm", stats.home_dangerous_attack_half1, stats.away_dangerous_attack_half1, stats.home_dangerous_attack_half1 + stats.away_dangerous_attack_half1],
-                        ["Góc", stats.home_corners_half1, stats.away_corners_half1, stats.home_corners_half1 + stats.away_corners_half1]
-                    ]
-                }).forceRender();
+                
 
-                tableHalf2.updateConfig({
-                    data: [
-                        [`Tổng (${total_stats_indices})`, stats.total_home_stats_half2, stats.total_away_stats_half2, stats.total_home_stats_half2 + stats.total_away_stats_half2],
-                        ["Tấn công nguy hiểm", stats.home_dangerous_attack_half2, stats.away_dangerous_attack_half2, stats.home_dangerous_attack_half2 + stats.away_dangerous_attack_half2],
-                        ["Góc", stats.home_corners_half2, stats.away_corners_half2, stats.home_corners_half2 + stats.away_corners_half2]
-                    ]
-                }).forceRender();
+                if(current_time<=45 || document.querySelector(`#${table_id_half1}`).innerHTML.toLowerCase().includes("loading")){
+                    tableHalf1.updateConfig({
+                        data: [
+                            [`Tổng (${total_stats_indices})`, stats.total_home_stats_half1, stats.total_away_stats_half1, stats.total_home_stats_half1 + stats.total_away_stats_half1],
+                            ["Tấn công nguy hiểm", stats.home_dangerous_attack_half1, stats.away_dangerous_attack_half1, stats.home_dangerous_attack_half1 + stats.away_dangerous_attack_half1],
+                            ["Góc", stats.home_corners_half1, stats.away_corners_half1, stats.home_corners_half1 + stats.away_corners_half1]
+                        ], columns: [
+                            { name: "Chỉ số", id:"index", width: "70px" },
+                            { name: response.team_home, id:"team_home", width: "90px" },
+                            { name: response.team_away, id:"team_away", width: "90px" },
+                            { name: "Tổng", id:"total"}
+                        ],
+                    }).forceRender();
+                } else {
+                    tableHalf2.updateConfig({
+                        data: [
+                            [`Tổng (${total_stats_indices})`, stats.total_home_stats_half2, stats.total_away_stats_half2, stats.total_home_stats_half2 + stats.total_away_stats_half2],
+                            ["Tấn công nguy hiểm", stats.home_dangerous_attack_half2, stats.away_dangerous_attack_half2, stats.home_dangerous_attack_half2 + stats.away_dangerous_attack_half2],
+                            ["Góc", stats.home_corners_half2, stats.away_corners_half2, stats.home_corners_half2 + stats.away_corners_half2]
+                        ], columns: [
+                            { name: "Chỉ số", id:"index", width: "70px" },
+                            { name: response.team_home, id:"team_home", width: "90px" },
+                            { name: response.team_away, id:"team_away", width: "90px" },
+                            { name: "Tổng", id:"total"}
+                        ]
+                    }).forceRender();
+                }
             };
 
             // Update tables every 5 seconds
